81274c261658218d852db1de4880e90a7b67b069
someCatInTheWorld <162684669+someCatInTheWorld@users.noreply.github.com>
2026-06-03 20:15:46 +1000

    Indent hell :P

--- patch.py
+++ patch.py
@@ -21,9 +21,9 @@ for path in glob.glob('scratch-gui/build/**/*.js', recursive=True):
 
 # Yes, this is *very* disgusting, but this is just an archive.
 fetchFile = glob.glob('scratch-gui/build/js/editor~embed~fullscreen~player.*.js')[0]
-  print(f'Patching JS {path}')
-  with open(fetchFile, 'r') as f:
-    contents = f.read()
-    contents = contents.replace('e=>{if("<"!==', 'e=>{if("\n"!==')
-  with open(fetchFile, 'w') as f:
-    f.write(contents)
+print(f'Patching JS {fetchFile}')
+with open(fetchFile, 'r') as f:
+  contents = f.read()
+  contents = contents.replace('e=>{if("<"!==', 'e=>{if("\n"!==')
+with open(fetchFile, 'w') as f:
+  f.write(contents)
