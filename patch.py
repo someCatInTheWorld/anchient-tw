947adc6c9a538932d4c43b9e97b7087a511c0db3
someCatInTheWorld <162684669+someCatInTheWorld@users.noreply.github.com>
2026-06-03 20:04:19 +1000

    Ancient code, smelly patches!
    Those two go hand in hand. I wouldn't do this in any project that I'm serious about though.

--- patch.py
+++ patch.py
@@ -18,3 +18,12 @@ for path in glob.glob('scratch-gui/build/**/*.js', recursive=True):
     contents = contents.replace('https://trampoline.turbowarp.org', 'https://trampoline.turbowarp.xyz')
   with open(path, 'w') as f:
     f.write(contents)
+
+# Yes, this is *very* disgusting, but this is just an archive.
+fetchFile = glob.glob('scratch-gui/build/js/editor~embed~fullscreen~player.*.js')[0]
+  print(f'Patching JS {path}')
+    with open(fetchFile, 'r') as f:
+      contents = f.read()
+      contents = contents.replace('e=>{if("<"!==', 'e=>{if("\n"!==')
+    with open(fetchFile, 'w') as f:
+      f.write(contents)
