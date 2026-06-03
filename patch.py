e21eb3a2f0870300613a4d6e690ea163fae29175
someCatInTheWorld <162684669+someCatInTheWorld@users.noreply.github.com>
2026-06-03 18:58:26 +1000

    Third times the charm?
    P.S: Copilot tried to shove its slop commit message on me. ToT

--- patch.py
+++ patch.py
@@ -6,8 +6,8 @@ for path in glob.glob('scratch-gui/build/*.html'):
   print(f'Patching HTML {path}')
   with open(path, 'r') as f:
     contents = f.read()
+    # Sneaky GarboMuffin... I'll respect this for now.
     contents = contents.replace('</head>', '<meta name="robots" content="noindex"></head>')
-    contents = contents.replace('<link rel="manifest" href="manifest.webmanifest">', '')
   with open(path, 'w') as f:
     f.write(contents)
 
@@ -18,11 +18,3 @@ for path in glob.glob('scratch-gui/build/**/*.js', recursive=True):
     contents = contents.replace('https://trampoline.turbowarp.org', 'https://trampoline.turbowarp.xyz')
   with open(path, 'w') as f:
     f.write(contents)
-
-os.remove('scratch-gui/build/sw.js')
-os.remove('scratch-gui/build/manifest.webmanifest')
-os.remove('scratch-gui/build/fullscreen.html')
-os.remove('scratch-gui/build/index.html')
-
-shutil.copy('scratch-gui/build/editor.html', 'scratch-gui/build/index.html')
-shutil.copy('robots.txt', 'scratch-gui/build/robots.txt')
